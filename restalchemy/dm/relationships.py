47b9d36804ed9d240e425a62bc570a51e2bf3cea
Eugene Frolov <eugene@frolov.net.ru>
2014-04-17 16:44:54 +0400

    Add relationship for Domain Model
    Change-Id: If00fd8c1a53e15f4bef837f05cca3e96b0ddf53d

---
+++ restalchemy/dm/relationships.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 Eugene Frolov <eugene@frolov.net.ru>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from restalchemy.common import exceptions as exc
+from restalchemy.dm import properties
+
+
+class BaseRelationship(properties.AbstractProperty):
+    pass
+
+
+def relationship(*args, **kwargs):
+
+    class Relationship(BaseRelationship):
+
+        def __init__(self):
+            self._read_only = kwargs.pop('read_only', False)
+            self._required = kwargs.pop('required', False)
+            self._models = args
+            self._value = None
+
+        def _safe_value(self, value):
+            if isinstance(value, self._models):
+                return value
+            raise exc.ValueError(classname=self._models, value=value)
+
+        def restore_value(self, value):
+            self._value = self._safe_value(value)
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, value):
+            if self.is_read_only():
+                raise exc.ReadOnlyPropertyError
+            self._value = self._safe_value(value)
+
+        def is_read_only(self):
+            return self._read_only
+
+        def check(self):
+            if self._required and self.value is None:
+                raise exc.ValueRequiredError()
+
+    return Relationship
