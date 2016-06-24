893fa9736e897d50d61e1cf2b801538881e097d5
Eugene Frolov <efrolov@mirantis.com>
2016-06-25 00:23:51 +0300

    Add MySQLEngine and Session classes

--- restalchemy/storage/sql/engines.py
+++ restalchemy/storage/sql/engines.py
@@ -17,11 +17,32 @@
 #    under the License.
 
 import abc
+import contextlib
 import urlparse
 
+from mysql.connector import pooling
 import six
 
 from restalchemy.common import singletons
+
+
+class Session(object):
+
+    def __init__(self, conn):
+        self._conn = conn
+        self._cursor = conn.cursor()
+
+    def execute(self, statement, values):
+        return self._cursor.execute(statement, values)
+
+    def rollback(self):
+        self._conn.rollback()
+
+    def commit(self):
+        self._conn.commit()
+
+    def close(self):
+        self._conn.close()
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -48,12 +69,74 @@ class AbstractEngine(object):
         raise NotImplementedError()
 
 
+class MySQLEngine(AbstractEngine):
+
+    URL_SCHEMA = 'mysql'
+
+    def __init__(self, db_url, config=None):
+        super(MySQLEngine, self).__init__(db_url)
+        if self._db_url.scheme != self.URL_SCHEMA:
+            raise ValueError("Database url should be starts with mysql://. "
+                             "For example: mysql://username:password@"
+                             "127.0.0.1:3306/database_name")
+        config = config or {}
+        config.update({
+            'user': self.db_username,
+            'password': self.db_password,
+            'database': self.db_name,
+            'host': self.db_host,
+            'port': self.db_port
+        })
+        self._pool = pooling.MySQLConnectionPool(**config)
+
+    @property
+    def db_name(self):
+        return self._db_url.path[1:]
+
+    @property
+    def db_username(self):
+        return self._db_url.username
+
+    @property
+    def db_password(self):
+        return self._db_url.password
+
+    @property
+    def db_host(self):
+        return self._db_url.hostname
+
+    @property
+    def db_port(self):
+        return self._db_url.port or 3306
+
+    def get_connection(self):
+        return self._pool.get_connection()
+
+    def get_session(self):
+        return Session(self.get_connection())
+
+    @property
+    @contextlib.contextmanager
+    def ctx_session(self):
+        session = Session(self.get_connection())
+        try:
+            yield session
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+
 class EngineFactory(singletons.InheritSingleton):
 
     def __init__(self):
         super(EngineFactory, self).__init__()
         self._engine = None
-        self._engines_map = {}
+        self._engines_map = {
+            MySQLEngine.URL_SCHEMA: MySQLEngine
+        }
 
     def configure_factory(self, db_url, config=None):
         """Configure_factory
